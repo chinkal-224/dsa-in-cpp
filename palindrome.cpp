d53a54dee882c7f45120ad0b6ac7d0c349f30050
Chinkal Malviya <cchinkalmalviya224@gmail.com>
2026-04-27 20:48:25 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+bool palindrome(int n)
+{
+
+    int original=n;
+    int reverse=0;
+    while(n>0){
+        int digit=n%10;
+        reverse=reverse*10+digit;
+        n=n/10;
+    }
+    if(original==reverse){
+        return true;
+    }else{
+        return false;
+    }
+}
