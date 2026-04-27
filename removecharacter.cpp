007bb682a6192240412fd2555902d0d049151c0c
Chinkal Malviya <cchinkalmalviya224@gmail.com>
2026-04-27 20:08:10 +0530

    Add files via upload

---
+++ removecharacter.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void removeAllOccurrencesOfChar(char input[], char c) {
+    
+    for(int i = 0; input[i] != '\0'; i++) {
+        
+        if(input[i] == c) {
+            
+            
+            for(int j = i; input[j] != '\0'; j++) {
+                input[j] = input[j + 1];
+            }
+            
+            i--;   
+        }
+    }
+}
+
+
+/*Problem statement
+For a given a string(str) and a character X, write a function to remove all the occurrences of X from the given string.
+The input string will remain unchanged if the given character(X) doesn't exist in the input string.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+aabccbaa
+a
+Sample Output 1:
+bccb
+Sample Input 2:
+xxyyzxx
+y
+Sample Output 2:
+xxzxx */
