737cc0dd921035815b7e3f344f505015b54818e6
Chinkal Malviya <cchinkalmalviya224@gmail.com>
2026-05-03 22:04:37 +0530

    Add files via upload

---
+++ unique.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int findUnique(int *arr, int size)
+{
+    int ans = 0;
+    for(int i = 0; i < size; i++) {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 1, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << "The unique element is: " << findUnique(arr, size) << endl;
+    return 0;
+}
