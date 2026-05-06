6df2c99cdfd8e2c0445eba0fe75512f54104c3fa
Chinkal Malviya <cchinkalmalviya224@gmail.com>
2026-05-06 20:31:31 +0530

    Add files via upload

---
+++ trappingrainwater.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+long long getTrappedWater(long long* arr, int n) {
+    int left = 0, right = n - 1;
+    long long leftMax = 0, rightMax = 0;
+    long long water = 0;
+
+    while (left <= right) {
+        if (arr[left] <= arr[right]) {
+            if (arr[left] >= leftMax) {
+                leftMax = arr[left];
+            } else {
+                water += leftMax - arr[left];
+            }
+            left++;
+        } else {
+            if (arr[right] >= rightMax) {
+                rightMax = arr[right];
+            } else {
+                water += rightMax - arr[right];
+            }
+            right--;
+        }
+    }
+    return water;
+}
